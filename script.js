eb15636e9bb53c621192086e7344b7db019cbf6a
Harry-Joseph9387 <harryjoseph9387@gmail.com>
2024-07-04 00:00:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const form=document.querySelector('form');
-const button=form.querySelector('input');
-
+const form=document.querySelector("form");
+const button=form.querySelector("button");
 function isValidEmail(email) {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
 }
 function checkemail(e){
     e.preventDefault();
-    const email=e.target.querySelector('input').value;
-
+    const email=e.target.querySelector('input').value
+    const element=form.querySelector("p");
+   
     if(isValidEmail(email)){
-        const element1=form.querySelector('p');
-        form.classList.remove('error');
-        form.removeChild(element1);
+        if(element!=null){
+        form.classList.remove("error");
+        form.removeChild(element);
+        }
     }
     else{
-        const element=document.createElement('p');
-        element.textContent="Enter a valid email";
-        form.insertBefore(element,button);
-        form.classList.add('error');
+        if(element==null){
+            console.log('fweohsdal')
+            const p=document.createElement('p');
+            p.textContent="Enter a valid email";
+            form.insertBefore(p,button);
+            form.classList.add('error')
+        }
+        
     }
 }
-form.addEventListener('submit',checkemail);
+form.addEventListener('submit',checkemail)
